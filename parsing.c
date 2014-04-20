0cce916247ad381055ee0f88fbb825219d4083e3
Ayberk Tosun <ayberk.tosun@gmail.com>
2014-04-20 16:43:34 +0300

    Refactor code

--- parsing.c
+++ parsing.c
@@ -9,9 +9,9 @@ static char buffer[2048];
 char* readline(char* prompt) {
   fputs(prompt, stdout);
   fgets(buffer, 2048, stdin);
-  char* cpy = malloc(strlen(buffer)+1);
+  char* cpy = malloc(strlen(buffer) + 1);
   strcpy(cpy, buffer);
-  cpy[strlen(cpy)-1] = '\0';
+  cpy[strlen(cpy) - 1] = '\0';
   return cpy;
 }
 
@@ -20,11 +20,10 @@ void add_history(char* unused) {}
 #else
 
 #include <editline/readline.h>
-// #include <editline/history.h>
 
 #endif
 
-/* Use operator string to see which operation to perform */
+// Use operator string to see which operation to perform
 long eval_op(long x, char* op, long y) {
   if (strcmp(op, "+") == 0) { return x + y; }
   if (strcmp(op, "-") == 0) { return x - y; }
@@ -34,17 +33,16 @@ long eval_op(long x, char* op, long y) {
 }
 
 long eval(mpc_ast_t* t) {
+  // If tagged as number return it directly, otherwise expression
+  if (strstr(t->tag, "number")) { return atoi(t->contents); }
 
-  /* If tagged as number return it directly, otherwise expression. */
-  if (strstr(t->tag, "number")) {return atoi(t->contents); }
-
-  /* The operator is always second child. */
+  // The operator is always the second child
   char* op = t->children[1]->contents;
 
-  /* We store the third child in `x` */
+  // We store the third child in `x`
   long x = eval(t->children[2]);
 
-  /* Iterate the remaining children, combining using our operator*/
+  // Iterate the remaining children, combining using our operator
   int i = 3;
   while (strstr(t->children[i]->tag, "expr")) {
     x = eval_op(x, op, eval(t->children[i]));
@@ -54,55 +52,43 @@ long eval(mpc_ast_t* t) {
   return x;
 }
 
-
-
 int main(int argc, char** argv) {
-  
-  /* Create Some Parsers */
-  mpc_parser_t* Number   = mpc_new("number");
+  mpc_parser_t* Number = mpc_new("number");
   mpc_parser_t* Operator = mpc_new("operator");
-  mpc_parser_t* Expr     = mpc_new("expr");
-  mpc_parser_t* Lispy    = mpc_new("lispy");
-  
-  /* Define them with the following Language */
+  mpc_parser_t* Expr = mpc_new("expr");
+  mpc_parser_t* Lispy = mpc_new("lispy");
+
   mpca_lang(MPC_LANG_DEFAULT,
-    "                                                     \
-      number   : /-?[0-9]+/ ;                             \
-      operator :  '+' | '-'   | '*'   | '/'   |            \
-                'add' | 'sub' | 'mul' | 'div' ;            \
-      expr     : <number> | '(' <operator> <expr>+ ')' ;  \
-      lispy    : /^/ <operator> <expr>+ /$/ ;             \
-    ",
-    Number, Operator, Expr, Lispy);
-  
-  puts("Lispy Version 0.0.0.0.2");
+  "                                                     \
+    number   : /-?[0-9]+/ ;                             \
+    operator : '+' | '-' | '*' | '/' ;                  \
+    expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+    lispy    : /^/ <operator> <expr>+ /$/ ;             \
+  ",
+  Number, Operator, Expr, Lispy);
+
+  puts("Tosun Lisp version 0.0.0.0.3");
   puts("Press Ctrl+c to Exit\n");
-  
+
   while (1) {
-  
-    char* input = readline("lispy> ");
+    char* input = readline("tosun-lisp> ");
     add_history(input);
-    
-    /* Attempt to parse the user input */
+
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispy, &r)) {
-      /* On success print and delete the AST */
+
       long result = eval(r.output);
-      printf("%li", result);
-      mpc_ast_delete(r.output);
+      printf("%li\n", result);
+      mpc_ast_delete(r.output);     
     } else {
-      /* Otherwise print and delete the Error */
       mpc_err_print(r.error);
       mpc_err_delete(r.error);
     }
-    
+
     free(input);
   }
-  
-  /* Undefine and delete our parsers */
+
   mpc_cleanup(4, Number, Operator, Expr, Lispy);
-  
+
   return 0;
 }
-
-
