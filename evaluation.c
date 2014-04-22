d655fbe9f13c840f32efaf9057545c203b2a7aa6
Ayberk Tosun <ayberk.tosun@gmail.com>
2014-04-22 20:57:43 +0300

    Lval read methods

--- evaluation.c
+++ evaluation.c
@@ -41,7 +41,7 @@ typedef struct lval {
 enum { LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_SEXPR };
 
 // Create enumeration of possible error types
-enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+// enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
 
 // Construct a new pointer to a new Number lval
 lval* lval_num(long x) {
@@ -65,7 +65,17 @@ lval* lval_sym(char* s) {
     lval* v = malloc(sizeof(lval));
     v->type = LVAL_SYM;
     v->sym = malloc(strlen(s) + 1);
-    strcpy(v->err, s);
+    strcpy(v->sym, s);
+    return v;
+}
+
+
+// A pointer to a new empty Sexpr lval
+lval* lval_sexpr(void) {
+    lval *v = malloc(sizeof(lval));
+    v->type = LVAL_SEXPR;
+    v->count = 0;
+    v->cell = NULL;
     return v;
 }
 
@@ -93,19 +103,11 @@ void lval_del(lval* v) {
 }
 
 
-// A pointer to a new empty Sexpr lval
-lval* lval_sexpr(void) {
-    lval *v = malloc(sizeof(lval));
-    v->type = LVAL_SEXPR;
-    v->count = 0;
-    v->cell = NULL;
-    return v;
-}
 
 lval* lval_add(lval* v, lval* x) {
     v->count++;
     v->cell = realloc(v->cell, sizeof(lval*) *  v->count);
-    v->cell[v->count] = x;
+    v->cell[v->count - 1] = x;
     return v;
 }
 
@@ -122,16 +124,16 @@ lval* lval_read(mpc_ast_t* t) {
 
     // If root (>) or sexpr then create empty list
     lval* x = NULL;
-    if (strcmp(t->tag, ">")== 0) { x = lval_sexpr(); }
-    if (strcmp(t->tag, "sexpr")) { x = lval_sexpr(); }
+    if (strcmp(t->tag, ">") == 0) { x = lval_sexpr(); }
+    if (strstr(t->tag, "sexpr")) { x = lval_sexpr(); }
 
     // Fill this list with any valid expression contained within
     for (int i = 0; i < t->children_num; i++) {
         if (strcmp(t->children[i]->contents, "(") == 0) { continue; }
         if (strcmp(t->children[i]->contents, ")") == 0) { continue; }
-        if (strcmp(t->children[i]->contents, "{") == 0) { continue; }
         if (strcmp(t->children[i]->contents, "}") == 0) { continue; }
-        if (strcmp(t->children[i]->tag, "regex") == 0) { continue; }
+        if (strcmp(t->children[i]->contents, "{") == 0) { continue; }
+        if (strcmp(t->children[i]->tag,  "regex") == 0) { continue; }
         x = lval_add(x, lval_read(t->children[i]));
     }
 
@@ -238,11 +240,11 @@ int main(int argc, char** argv) {
 
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r)) {
-            // lval result = eval(r.output);
-            lval* x = lval_read(r.output);
+            lval *x = lval_read(r.output);    
             lval_println(x);
             lval_del(x);
-            mpc_ast_delete(r.output);     
+
+            mpc_ast_delete(r.output);  
         } else {
             mpc_err_print(r.error);
             mpc_err_delete(r.error);
