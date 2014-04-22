a3ffabaf3caa68e6af89574bb6973ff17747cd48
Ayberk Tosun <ayberk.tosun@gmail.com>
2014-04-22 15:48:06 +0300

    Add sexprs to parser config

--- evaluation.c
+++ evaluation.c
@@ -116,18 +116,20 @@ lval eval(mpc_ast_t* t) {
 }
 int main(int argc, char** argv) {
     mpc_parser_t* Number = mpc_new("number");
-    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Symbol = mpc_new("symbol");
+    mpc_parser_t* Sexpr = mpc_new("sexpr");
     mpc_parser_t* Expr = mpc_new("expr");
     mpc_parser_t* Lispy = mpc_new("lispy");
 
     mpca_lang(MPC_LANG_DEFAULT,
         "                                                     \
         number   : /-?[0-9]+/ ;                             \
-        operator : '+' | '-' | '*' | '/' | '\%' ;                  \
-        expr     : <number> | '(' <operator> <expr>+ ')' ;  \
-        lispy    : /^/ <operator> <expr>+ /$/ ;             \
+        operator : '+' | '-' | '*' | '/' | '\%' ;           \
+        sexpr    : '(' <expr>* ')';                         \
+        expr     : <number> | <symbol> | <sexpr> ;  \
+        lispy    : /^/ <expr>* /$/ ;             \
         ",
-    Number, Operator, Expr, Lispy);
+    Number, Symbol, Sexpr, Expr, Lispy);
 
     puts("Tosun Lisp version 0.0.0.0.3");
     puts("Press Ctrl+c to Exit\n");
@@ -149,7 +151,7 @@ int main(int argc, char** argv) {
         free(input);
     }
 
-    mpc_cleanup(4, Number, Operator, Expr, Lispy);
+    mpc_cleanup(4, Number, Symbol, Sexpr, Expr, Lispy);
 
     return 0;
 }
